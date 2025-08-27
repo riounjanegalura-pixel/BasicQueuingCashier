12bc4e7c003b2fa8d16d8f3777575fe102b61f73
Rio Unjane Galura <riounjanegalura@gmail.com>
2025-08-27 17:15:51 +0800

    updated customer view class

--- CustomerView.Designer.cs
+++ CustomerView.Designer.cs
@@ -28,7 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblCurrentNumber = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(248, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(277, 48);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "*Now serving";
+            // 
+            // lblCurrentNumber
+            // 
+            this.lblCurrentNumber.AutoSize = true;
+            this.lblCurrentNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCurrentNumber.Location = new System.Drawing.Point(324, 183);
+            this.lblCurrentNumber.Name = "lblCurrentNumber";
+            this.lblCurrentNumber.Size = new System.Drawing.Size(109, 39);
+            this.lblCurrentNumber.TabIndex = 1;
+            this.lblCurrentNumber.Text = "label2";
             // 
             // CustomerView
             // 
@@ -36,12 +58,19 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightSlateGray;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblCurrentNumber);
+            this.Controls.Add(this.label1);
             this.Name = "CustomerView";
             this.Text = "CustomerView";
+            this.Load += new System.EventHandler(this.CustomerView_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblCurrentNumber;
     }
 }
--- CustomerView.cs
+++ CustomerView.cs
@@ -10,11 +10,32 @@ using System.Windows.Forms;
 
 namespace BasicQueuingCashier
 {
-    public partial class CustomerView: Form
+    public partial class CustomerView : Form
     {
         public CustomerView()
         {
             InitializeComponent();
+
+            UpdateQueueDisplay();
+
+            CashierClass.OnQueueChanged += UpdateQueueDisplay;
+        }
+
+        private void CustomerView_Load(object sender, EventArgs e)
+        {
+            CashierClass.OnQueueChanged -= UpdateQueueDisplay;
+        }
+
+        private void UpdateQueueDisplay()
+        {
+            if (CashierClass.CashierQueue != null && CashierClass.CashierQueue.Count > 0)
+            {
+                lblCurrentNumber.Text = CashierClass.CashierQueue.Peek();
+            }
+            else
+            {
+                lblCurrentNumber.Text = "           ";
+            }
         }
     }
 }
